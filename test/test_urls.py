3c7fe0e4d84357a21fb73d68691edca4453e10c2
bymse <egorstrelov@gmail.com>
2021-11-28 00:51:58 +0300

    Handlers test. Create if url doesnt exist

--- test/test_urls.py
+++ test/test_urls.py
@@ -14,3 +14,16 @@ class UrlsHandlersTestCase(unittest.TestCase):
         alphanum_code = 'qwe123'
         store_patch.return_value = alphanum_code
         self.assertEqual(handlers.handle_create(existing_url), f'{base_url}/s/{alphanum_code}')
+
+    @patch('bys.urls.store.save_url')
+    @patch('bys.urls.url_code.get_url_codes')
+    @patch('bys.urls.store.find_alphanum_code', return_value=None)
+    def test_should_save_url_if_doesnt_exist(self, store_find_patch, get_url_codes_patch, store_save_patch, _):
+        input_url = 'https://example.com/test/url'
+        num_code = 123
+        alphanum_code = 'qwe'
+
+        get_url_codes_patch.return_value = (num_code, alphanum_code)
+        handlers.handle_create(input_url)
+
+        store_save_patch.assert_called_once_with(num_code, alphanum_code, input_url)

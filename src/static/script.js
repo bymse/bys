65076d48c49900b4d4f290b96472c8e1ff7f8369
bymse <egorstrelov@gmail.com>
2022-01-17 00:12:35 +0300

    Js toggle + css fixes

--- src/static/script.js
+++ src/static/script.js
@@ -1,10 +1,22 @@
-const copyButton = document.getElementById('copy-button');
+const copyButton = document.getElementById("copy-button");
 if (copyButton) {
-  copyButton.addEventListener('click', () => {
-    const shortUrl = document.getElementById('short-url');
+  copyButton.addEventListener("click", () => {
+    const shortUrl = document.getElementById("short-url");
     if (shortUrl) {
       navigator.clipboard.writeText(shortUrl.href);
-      copyButton.innerText = "Copied!"
+      copyButton.innerText = "Copied!";
     }
   });
 }
+
+const about = document.getElementById("about");
+about.addEventListener("click", () => {
+  const readme = document.getElementById("readme");
+  const hasHide = readme.classList.contains("hide");
+  readme.classList.toggle("hide");
+  if (hasHide) {
+    about.innerHTML = "&#9660; About"
+  } else {
+    about.innerHTML = "&#9658; About";
+  }
+});
